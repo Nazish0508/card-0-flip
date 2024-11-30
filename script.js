8eaad91098aea9b1aa10484fdc9325606b84e551
Nazish Ansari <ansarinaz.2104@gmail.com>
2024-11-30 23:57:30 +0530

    created necessary files and added images

---
+++ script.js
@@ -0,0 +1,11 @@
+function startGame() {
+  alert("Game Started!");
+}
+startGame();
+
+let stb = document.querySelector('str-btn');
+
+stb.addEventListener('onclick', ()=>{
+  stb.animate(keyframes)
+})
+
