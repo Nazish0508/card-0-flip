2a3da0bc8f8b80d2e73776dde644c21c283c1aad
Nazish Ansari <ansarinaz.2104@gmail.com>
2024-12-01 16:24:00 +0530

    completed start page

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-function startGame() {
-  alert("Game Started!");
-}
-startGame();
+// function startGame() {
+//   alert("Game Started!");
+// }
+// startGame();
 
-let stb = document.querySelector('str-btn');
+let stb = document.querySelector('#str-btn');
+let startscr = document.querySelector('#start-screen');
+let gamescr = document.querySelector('.game-screen');
+let homeimg = document.querySelector('#homeimg');
+let title = document.querySelector('#title');
 
-stb.addEventListener('onclick', ()=>{
-  stb.animate(keyframes)
-})
+
+stb.addEventListener('click', ()=>{
+  title.classList.add("animate__flipOutX");
+  homeimg.classList.add("animate__flipOutX");
+  stb.classList.add("animate__flipOutX");
+
+  setTimeout(() =>{
+    startscr.classList.add("hidden");
+    gamescr.classList.remove("hidden");
+    gamescr.classList.add("animate__flipInX");
+  }, 1000);  
+});
+
 
