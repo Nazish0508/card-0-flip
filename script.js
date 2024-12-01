fb184a63e441695021693f62d23e3b847786debb
Nazish Ansari <ansarinaz.2104@gmail.com>
2024-12-01 22:49:54 +0530

    changed everything

--- script.js
+++ script.js
@@ -3,23 +3,25 @@
 // }
 // startGame();
 
-let stb = document.querySelector('#str-btn');
-let startscr = document.querySelector('#start-screen');
-let gamescr = document.querySelector('.game-screen');
-let homeimg = document.querySelector('#homeimg');
-let title = document.querySelector('#title');
+// let stb = document.querySelector('#str-btn');
+// let startscr = document.querySelector('#start-screen');
+// let gamescr = document.querySelector('.game-screen');
+// let homeimg = document.querySelector('#homeimg');
+// let title = document.querySelector('#title');
 
 
-stb.addEventListener('click', ()=>{
-  title.classList.add("animate__flipOutX");
-  homeimg.classList.add("animate__flipOutX");
-  stb.classList.add("animate__flipOutX");
+// stb.addEventListener('click', ()=>{
+//   title.classList.add("animate__flipOutX");
+//   homeimg.classList.add("animate__flipOutX");
+//   stb.classList.add("animate__flipOutX");
 
-  setTimeout(() =>{
-    startscr.classList.add("hidden");
-    gamescr.classList.remove("hidden");
-    gamescr.classList.add("animate__flipInX");
-  }, 1000);  
-});
+//   setTimeout(() =>{
+//     startscr.classList.add("hidden");
+//     gamescr.classList.remove("hidden");
+//     gamescr.classList.add("animate__flipInX");
+//   }, 1000);  
+// });
+
+const cards = 
 
 
