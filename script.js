fa6c1a284e9cf55cae13fc32c8bda381854122ac
Nazish Ansari <ansarinaz.2104@gmail.com>
2024-12-02 12:06:15 +0530

    Completed the project

--- script.js
+++ script.js
@@ -1,27 +1,82 @@
-// function startGame() {
-//   alert("Game Started!");
-// }
-// startGame();
+const cards = document.querySelectorAll('.memory-card');
 
-// let stb = document.querySelector('#str-btn');
-// let startscr = document.querySelector('#start-screen');
-// let gamescr = document.querySelector('.game-screen');
-// let homeimg = document.querySelector('#homeimg');
-// let title = document.querySelector('#title');
+let hasDLippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let score = 0;
+
+function flipCard() {
+  if(lockBoard) return;
+  if(this === firstCard) return;
+
+  this.classList.toggle('flip');
+
+  if(!hasDLippedCard){
+    //first click
+    hasDLippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+    hasDLippedCard = false;
+    secondCard = this;
+
+    //do cards match?
+    checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch =  firstCard.dataset.framework === secondCard.dataset.framework;
+   
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  score++;
+  console.log('Score incremented:', score);
+
+  document.querySelector('.score').textContent = `Score: ${score}`;
+
+  checkWinWithScore();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard(){
+  hasDLippedCard = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+}
+
+(function shuffle() {
+  cards.forEach(card =>{
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+function checkWinWithScore() {
+  if (score === 8) {
+    setTimeout(() => {
+      alert("You won!");
+    }, 500); // Delay to allow the last flip animation
+  }
+}
 
 
-// stb.addEventListener('click', ()=>{
-//   title.classList.add("animate__flipOutX");
-//   homeimg.classList.add("animate__flipOutX");
-//   stb.classList.add("animate__flipOutX");
-
-//   setTimeout(() =>{
-//     startscr.classList.add("hidden");
-//     gamescr.classList.remove("hidden");
-//     gamescr.classList.add("animate__flipInX");
-//   }, 1000);  
-// });
-
-const cards = 
+cards.forEach(card => card.addEventListener('click', flipCard));
 
 
